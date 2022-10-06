c2098e30e6b65a1a23b7ff87063ce492f431e4f1
Denis Khodakov <denis5517@yandex.ru>
2022-10-06 20:18:43 +0700

    repository

---
+++ homework-7.8/homework-7.8/homework-7.8/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_7._8
+{
+    internal class Repository
+    {
+        public Worker[] GetAllWorkers()
+        {
+            get { return Workers[index]; }                    // здесь происходит чтение из файла
+        }
+
+    }
+}
+
---
+++ homework-7.8/homework-7.8/homework-7.8/struct Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_7._8
+{
+    internal class struct_Worker
+    {
+        public int Id { get; set; }
+        public int DateTime { get; set; } 
+        public string FIO { get; set; }
+        public int age { get; set; }
+        public int growth { get; set; }
+        public int DateBirth { get; set; }
+        public string PlaceBirth { get; set; }
+    }
+}
