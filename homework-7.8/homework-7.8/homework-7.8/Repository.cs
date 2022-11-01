900c49d5b3681d6266604027dae8c2064a3bbcbc
Denis Khodakov <denis5517@yandex.ru>
2022-11-01 20:11:44 +0700

    Repository.cs

--- homework-7.8/homework-7.8/homework-7.8/Repository.cs
+++ homework-7.8/homework-7.8/homework-7.8/Repository.cs
@@ -6,13 +6,117 @@ using System.Threading.Tasks;
 
 namespace homework_7._8
 {
-    internal class Repository
+    class Repository
     {
-        public Worker[] GetAllWorkers()
+        private string path;
+        private int index;
+        Employee[] employees;
+
+        #region Конструктор
+        public Repository (string path)
         {
-            get { return Workers[index]; }                    // здесь происходит чтение из файла
+            this.path = path;
+            this.index = 0;
+            this.employees = new Employee[2];
         }
+        #endregion
+
+        #region Увеличивает размер хранилища
+        /// <summary>
+        /// Увеличивает размер хранилища
+        /// </summary>
+        /// <param name="isFull">Проверка заполнености</param>
+        private void Rezize(bool isFull)
+        {
+            if (isFull)
+            {
+                Array.Resize(ref this.employees, this.employees.Length * 2);
+            }
+            #endregion
+
+            Добавление нового работника в репозиторий
+
+            #region Вывести всех сотрудников в консоль
+            /// <summary>
+            /// Выводит репозиторий в консоль
+            /// </summary>
+            public void PrinTAll()
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    employees[i].Print();
+                }
+            }
+            #endregion
+
+            /// <summary>
+            /// Метод добавления массива записей в ежедневник
+            /// </summary>
+            /// <param name="isFull">Массив записей</param>
+            public void AddNotes(Note[] isFull)
+            {
+                int i = 0
+                while (i < isFull.Length)                              // Цикл добавления
+                {
+                    Resize(this.index >= this.note.Length);          // Изменение размера
+                    this.note[this.index] = isFull[i];               // Добавление элемента
+                    i += 1;
+                    this.index++;                                    // величение индекса
+                }
+            }
+
+            /// <summary>
+            /// Метод, удаляющий запись под конкретным индексом из массива
+            /// </summary>
+            /// <param name="index">Индекс записи </param>
+            public void DeleteNotes(int Index)
+            {
+                Array.Clear(this.note, index - 1, 1);           //Удаление одной записи из массива под конкретным индексом
+
+                for (int i = Index - 1; i < this.index - 1; i++)//Сдвиг записей назад на еденицу
+                    this.note[i] = this.note[i + 1];
+
+                this.index--;                                   //Уменьшение индекса
+            }
+            /// <summary>
+            /// Метод, проверяющий наличие файла
+            /// </summary>
+            /// <returns>Наличие фала</returns>
+            private bool CheckFile()
+            {
+                FileInfo fi = new FileInfo(path);
+
+                if (!fi.Exists) fi.Create().Close();    //Создание файла в случае его отсутствия
+
+                return fi.Exists;
+            }
+            /// <summary>
+            /// Метод, проверяющий попадание введеного индекса диапазону массива ежедневника
+            /// </summary>
+            /// <returns>Индекс</returns>
+            public int CheckIndex()
+            {
+                bool flag = false;
+                int Index = 0;
+
+                while (!flag)                                            //Цикл ввода и проверки
+                {
+                    flag = true;
+                    flag = int.TryParse(Console.ReadLine(), out Index); //Проверка введеного значения
+
+                    if (Index > this.index) flag = false;               //Проверка попадания в диапазон
+
+                    if (!flag)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Введите индекс из диапазона индекса записей!");
+                    }
+                }
+                return index;
+            }
+        }    
 
     }
+}
 }
 
--- homework-7.8/homework-7.8/homework-7.8/struct Worker.cs
+++ homework-7.8/homework-7.8/homework-7.8/struct Worker.cs
@@ -1,19 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace homework_7._8
 {
-    internal class struct_Worker
+    struct Worker
     {
-        public int Id { get; set; }
-        public int DateTime { get; set; } 
-        public string FIO { get; set; }
-        public int age { get; set; }
-        public int growth { get; set; }
-        public int DateBirth { get; set; }
-        public string PlaceBirth { get; set; }
+        public Book(string FIO, string PlaceBirth)
+            
+        {
+            this.id = ID;
+            this.datetime = DateTime.Now;
+            this.fio = FIO;
+            this.age = Age;
+            this.growth = Growth;
+            this.datebirth = DateBirth;
+            this.placebirth = PlaceBirth;
+        }
+
+        public string Print()
+        {
+            return string.Format(
+
+                id,
+                datetime,
+                fio,
+                age,
+                growth,
+                datebirth,
+                placebirth;)
+
+        }
+
+        public int Id { get { return this.id; } set { this.id = value; } }
+        public DateTime DateTime { get { return this.datetime; } set { this.datetime = value; } }
+        public string FIO { get { return this.fio; } set { this.fio = value; } }
+        public int Age { get { return this.age; } set { this.age = value; } }
+        public int Growth { get { return this.growth; } set { this.growth = value; } }
+        public int DateBirth { get { return this.datebirth; } set { this.datebirth = value; } }
+        public string PlaceBirth { get { return this.placebirth; } set { this.placebirth = value; } }
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+      
 }
+
+
+
+
